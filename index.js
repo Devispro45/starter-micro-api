a5456ac3e8309848481dd9a8de4fdc712682bf3a
Devispro45 <102111920+Devispro45@users.noreply.github.com>
2023-09-23 12:37:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
+const port = 8080;
+const proxyHost = 'example.com';
+const proxyPort = 80;
+
+const server = http.createServer((req, res) => {
+  const options = {
+    hostname: proxyHost,
+    port: proxyPort,
+    path: req.url,
+    method: req.method,
+    headers: req.headers,
+  };
+
+  const proxyReq = http.request(options, (proxyRes) => {
+    res.writeHead(proxyRes.statusCode, proxyRes.headers);
+    proxyRes.pipe(res);
+  });
+
+  req.pipe(proxyReq);
+});
+
+server.listen(port, () => {
+  console.log(`Proxy server is running on port ${port}`);
+});
